fee1f829facadfadc65c255f62c7c0cf0a2db08d
PrithiviAp <147227814+PrithiviAp@users.noreply.github.com>
2026-07-10 15:25:50 +0530

    Update CORS configuration to allow all origins

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:5173";
 
 const app = express();
-app.use(cors({ origin: CLIENT_ORIGIN }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.use("/api/rooms", roomRoutes);
