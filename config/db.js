c0cc4393c08dc4fec2e1bbe3fb0c503cf6f01f36
PrithiviAp <147227814+PrithiviAp@users.noreply.github.com>
2026-07-11 00:06:42 -0700

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://prithivi860_db_user:RKTdnDxCgKGiM4j2@chat-cluster-1.jbuzyje.mongodb.net/chat-app");
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
